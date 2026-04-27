e2aa99bebe147cded5e8b3a0f8f52eee1d522d85
abbaskhatri <m.abbaskhatri@gmail.com>
2026-04-27 16:41:22 -0500

    Remove snapshot secret artifacts

--- docs/snapshot_20251231_171322/bynkbook-web/src/lib/queries/useBusinesses.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { listBusinesses } from "@/lib/api/businesses";
-
-export function useBusinesses() {
-  return useQuery({
-    queryKey: ["businesses"],
-    queryFn: listBusinesses,
-  });
-}
